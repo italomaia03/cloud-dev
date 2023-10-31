fa5646616a5e8af1036f240bc7b5c2a002f8d3fd
Francisco Eliésio <69014925+fcoeliesio@users.noreply.github.com>
2023-10-31 12:15:50 -0300

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,11 +1,11 @@
--- Criação a tabela de equipamentos de laboratório de química.
+-- Criação da tabela de equipamentos de laboratório de química.
 CREATE TABLE equipes (
     id_equipe INT PRIMARY KEY,
     projeto VARCHAR(255) NOT NULL,
     status VARCHAR(255)
 );
 
--- Insira alguns dados iniciais
+-- Inserção de alguns dados iniciais
 INSERT INTO equipes (id_equipe, projeto, status)
 VALUES (1010, 'Projeto 1 ', 'Finalizado');
 
