3337f9d213a8b3a58afb06b73f60be51026680da
Italo F O Maia <italo.fernandes.om@gmail.com>
2023-10-31 00:25:53 -0300

    feat: get all registered lab technicians

--- app/app.py
+++ app/app.py
@@ -1,24 +1,31 @@
-from flask import Flask, json, jsonify
+from flask import Flask, jsonify
 import pymysql.cursors
 import os
-# REFATORAR PARA O NOVO BANCO DE DADOS
+
 app = Flask(__name__)
-# Rota para listar todos os equipamentos no banco de dados
+
+def bd_conn():
+    return pymysql.connect(
+        user=os.environ.get('DB_USER'),
+        password= os.environ.get('DB_PASSWORD'),
+        host='db',
+        database='scientialab',
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+@app.route('/')
+def index():
+    return '<h2>Hello, World!</h2>'
+
 @app.route('/equipes')
 def lista_equipes():
-    conn = pymysql.connect(
-        host='db',
-        user='root',
-        password='root',
-        database='database'
-    )
     
-    cursor = conn.cursor()
+    cursor = bd_conn().cursor()
     cursor.execute("SELECT * FROM equipes")
     result = cursor.fetchall()
-    conn.close()
+    cursor.close()
+    bd_conn().close()
 
-    # Formatar a saída como JSON
     lista_equipes = []
     for row in result:
         equipes = {
@@ -30,5 +37,26 @@ def lista_equipes():
 
     return jsonify(lista_equipes)
 
+def get_technicians():
+    with bd_conn().cursor() as cursor:
+        sql = "SELECT * FROM lab_technicians"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        cursor.close()
+    bd_conn().close()
+    return results
+
+@app.get('/techs')
+def get_lab_techs():
+    return jsonify({"Lab Technicians": get_technicians()})
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app_args = {}
+
+    app_args['host'] = os.environ.get('F_HOST') or "0.0.0.0"
+    app_args['port'] = os.environ.get('F_PORT') or 5000
+    app_args['debug'] = os.environ.get('F_DEBUG' or True)
+
+    app.run(**app_args)
+
+
